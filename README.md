aafff17d18d997998bf2177ee1f58c763f245b4a
Rokas <rokandroller@gmail.com>
2016-12-21 23:45:31 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Test
+
+> Skill test
+
+## Technologies
+
+- [**Gulp**](http://gulpjs.com)
+- [**Html**](https://developer.mozilla.org/es/docs/HTML/HTML5) 
+- [**Sass**](http://sass-lang.com)  
+- [**Babel**](https://babeljs.io)
+
+## Install and Use
+
+### Install
+
+```bash
+git clone https://github.com/rokart/Test.git
+npm install
+gulp
+```
+
+# Author 
+
+Rokas Valeckas - rokas@rokart.co.uk
+
+# License 
+
+The code is available under the **MIT** license. 
---
+++ src/scripts/index.js
@@ -0,0 +1,22 @@
+'use strict';
+//var $ = require('jQuery');
+
+//$('header h1').html('Works');
+
+//var ScrollMagic = require('scrollmagic');
+//var controller = new ScrollMagic.Controller();
+//
+//var scene = new ScrollMagic.Scene({
+//        triggerElement: "#trigger1"
+//    })
+//    .setTween("header h1", 0.5, {
+//        backgroundColor: "green"  
+//    })
+//
+//.addTo(controller);
+
+//new ScrollMagic.Scene({
+//    triggerElement: "#trigger1"
+//})
+//        .setClassToggle("header h1", "active") // add class toggle
+//        .addTo(controller);
